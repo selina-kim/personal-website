780841ebed1b13ab844dd1bb6b56294ed252b951
Selina <selinakim343@outlook.com>
2023-10-21 04:45:46 -0400

    chore: add prettier with astro + tailwind plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
@@ -8,5 +8,5 @@ import netlify from "@astrojs/netlify/functions";
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,36 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
-	theme: {
-		extend: {
-			colors: {
-				light: {
-					gray: 'var(--light-gray)',
-					main: 'var(--light-main)',
-				},
-				dark: {
-					gray: 'var(--dark-gray)',
-					main: 'var(--dark-main)',
-				},
-				accent: {
-					primary: 'var(--accent-primary)',
-					secondary: 'var(--accent-secondary)'
-				},
-			},
-			backgroundImage: {
-				'accent-gradient': 'var(--accent-gradient)',
-			},
-			screens: {
-				'sm': '640px',
-				'md': '768px',
-				'lg': '1024px',
-				'xl': '1280px',
-				'2xl': '1536px',
-				'3xl': '2000px'
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        light: {
+          gray: "var(--light-gray)",
+          main: "var(--light-main)",
+        },
+        dark: {
+          gray: "var(--dark-gray)",
+          main: "var(--dark-main)",
+        },
+        accent: {
+          primary: "var(--accent-primary)",
+          secondary: "var(--accent-secondary)",
+        },
+      },
+      backgroundImage: {
+        "accent-gradient": "var(--accent-gradient)",
+      },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+        "3xl": "2000px",
+      },
+    },
+  },
+  plugins: [],
+};
