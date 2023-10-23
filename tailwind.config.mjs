df18b04ad9e162e86b07bfef478aa66e85129118
Selina <selinakim343@outlook.com>
2023-10-23 11:12:38 -0400

    chore: modify tw config for font & breakpoints

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class",
@@ -22,13 +24,8 @@ module.exports = {
       backgroundImage: {
         "accent-gradient": "var(--accent-gradient)",
       },
-      screens: {
-        sm: "640px",
-        md: "768px",
-        lg: "1024px",
-        xl: "1280px",
-        "2xl": "1536px",
-        "3xl": "2000px",
+      fontFamily: {
+        sans: ["Red Hat Display", ...defaultTheme.fontFamily.sans],
       },
     },
   },
