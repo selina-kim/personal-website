cefd90b2a910f8b1ab204391c2a4d3f5e73b5a5c
Selina <selinakimdev@outlook.com>
2025-07-26 01:12:28 -0400

    chore: lint rule no escaped entities

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,9 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.config({
     extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+    rules: {
+      "react/no-unescaped-entities": "off",
+    },
   }),
 ];
 
